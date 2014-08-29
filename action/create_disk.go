df7d0870c13359ab0e9d0834da9075aabc714489
Kris Hicks <krishicks@gmail.com>
2014-08-29 16:47:45 -0700

    Use named type for create_disk cloud properties
    DiskCloudProperties, which is a map[string]interface instead of
    map[string]string.

--- action/create_disk.go
+++ action/create_disk.go
@@ -10,11 +10,13 @@ type CreateDisk struct {
 	diskCreator bwcdisk.Creator
 }
 
+type DiskCloudProperties map[string]interface{}
+
 func NewCreateDisk(diskCreator bwcdisk.Creator) CreateDisk {
 	return CreateDisk{diskCreator: diskCreator}
 }
 
-func (a CreateDisk) Run(size int, _ map[string]string, _ VMCID) (DiskCID, error) {
+func (a CreateDisk) Run(size int, _ DiskCloudProperties, _ VMCID) (DiskCID, error) {
 	disk, err := a.diskCreator.Create(size)
 	if err != nil {
 		return "", bosherr.WrapError(err, "Creating disk of size '%d'", size)
