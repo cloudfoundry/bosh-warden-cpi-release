8fea1b8be3e596b571e95b964f3e905c0393ea6e
Luke Woydziak <luke.woydziak@emc.com>
2015-09-08 16:42:40 -0700

    use dev version for building dev releases

--- ci/build-release.sh
+++ ci/build-release.sh
@@ -4,12 +4,14 @@ set -e -x
 
 mkdir out
 
+dev_version=`cat dev-version/number`
+
 cd bosh-warden-cpi-release
 
 # todo remove installation
 gem install bosh_cli --no-ri --no-rdoc
 
-bosh -n create release --with-tarball
+bosh -n create release --version $dev_version --with-tarball
 
 mv dev_releases/bosh-warden-cpi/*.tgz ../out/
 
