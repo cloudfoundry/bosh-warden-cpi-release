0d1c32debe2524f0d351a91344296a063fece63e
Luke Woydziak <luke.woydziak@emc.com>
2015-09-08 17:02:29 -0700

    since dev-version is used no need to remove +

--- ci/build-release.sh
+++ ci/build-release.sh
@@ -14,8 +14,3 @@ gem install bosh_cli --no-ri --no-rdoc
 bosh -n create release --version $dev_version --with-tarball
 
 mv dev_releases/bosh-warden-cpi/*.tgz ../out/
-
-cd ../out
-
-# todo s3 concourse resource does not properly handle plus sign
-mv *.tgz `echo *.tgz|tr '+dev' '-dev'`
