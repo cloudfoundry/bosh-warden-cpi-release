dc2c378392e4185148a408901b9157648ccccdec
Konstantin Kiess <kkiess@vmware.com>
2021-10-27 18:56:21 +0200

    extracted promote task into its own yaml. Updated script to use go bosh.

--- ci/promote-release.sh
+++ ci/promote-release.sh
@@ -1,35 +1,33 @@
 #!/usr/bin/env bash
 
 set -e -x
+integer_version=$(cat dev-version/number | cut -f1 -d'.')
+pushd bosh-warden-cpi-release
+  mkdir /tmp/bin
+  export PATH=${PATH}:/tmp/bin
+  wget -O /tmp/bin/bosh https://github.com/cloudfoundry/bosh-cli/releases/download/v$BOSH_CLI_VERSION/bosh-cli-$BOSH_CLI_VERSION-linux-amd64
+  chmod +x /tmp/bin/bosh
 
-cd bosh-warden-cpi-release
-
-# todo remove installation
-ls -la /root/.gem
-rm -rf /root/.gem
-gem install net-ssh -v 2.10.0.beta2
-gem install fog-google -v 0.1.0
-gem install bosh_cli --no-ri --no-rdoc
-
-cat > config/private.yml << EOF
+  cat > config/private.yml << EOF
 ---
 blobstore:
-  s3:
+  options:
     access_key_id: $BOSH_AWS_ACCESS_KEY_ID
     secret_access_key: $BOSH_AWS_SECRET_ACCESS_KEY
 EOF
 
-bosh finalize release `echo ../pipeline-bosh-warden-cpi-tarball/*.tgz`
+  bosh finalize-release --version $integer_version ../pipeline-bosh-warden-cpi-tarball/*.tgz
 
-# Be extra careful about not committing private.yml
-rm config/private.yml
+  # Be extra careful about not committing private.yml
+  rm config/private.yml
 
-final_version=`git diff releases/*/index.yml | grep -E "^\+.+version" | sed s/[^0-9]*//g`
-git diff | cat
-git add .
+  final_version=`git diff releases/*/index.yml | grep -E "^\+.+version" | sed s/[^0-9]*//g`
+  git diff | cat
+  git add .
 
-git config --global user.email "cf-bosh-eng@pivotal.io"
-git config --global user.name "CI"
-git commit -m "New final release v$final_version"
+  git config --global user.email "cf-bosh-eng@pivotal.io"
+  git config --global user.name "CI"
+  git commit -m "New final release v$final_version"
 
-echo $final_version > ../final_version
+  echo $final_version > ../final_version
+popd
