5af1b2c918b23c9044f2a774bbbc671d18278e1c
Camila Londoño <londonoc@vmware.com>
2022-04-06 19:28:49 -0700

    Deploy bats warden director in two steps
    To avoid having to have a static ip address, we first deploy an empty VM,
    get the ip address, then use that to deploy the director to that same VM
    
    Signed-off-by: Joseph Palermo <jpalermo@pivotal.io>

--- ci/bats.sh
+++ ci/bats.sh
@@ -5,7 +5,6 @@ set -e -x
 source $(dirname $0)/lib/bats.sh
 
 if $DEV_RELEASE; then
-
   git clone https://github.com/cloudfoundry/bosh-warden-cpi-release.git bosh-warden-cpi-release-dev
   pushd bosh-warden-cpi-release-dev
     cpi_release_path=`pwd`/dev-release.tgz
@@ -17,9 +16,11 @@ else
 fi
 
 #credhub login --skip-tls-validation
-stemcell_url=`cat warden-ubuntu-bionic-stemcell/url`
+warden_stemcell_url=`cat warden-ubuntu-bionic-stemcell/url`
+iaas_stemcell_url=`cat iaas-stemcell/url`
+iaas_stemcell_version=`cat iaas-stemcell/version`
 bosh_release_path=$PWD/bosh-release/*.tgz
 
 garden_linux_release_path=$PWD/garden-linux-release/*.tgz
 
-run_bats_on_vm $stemcell_url $bosh_release_path $cpi_release_path $garden_linux_release_path $SKIP_RUBY_INSTALL $BOSH_CLI_VERSION
+run_bats_on_vm $warden_stemcell_url $iaas_stemcell_url $iaas_stemcell_version $bosh_release_path $cpi_release_path $garden_linux_release_path $SKIP_RUBY_INSTALL $BOSH_CLI_VERSION
--- ci/lib/bats.sh
+++ ci/lib/bats.sh
@@ -7,15 +7,15 @@ chmod 600 ${jumpbox_private_key_path}
 echo "${JUMPBOX_PRIVATE_KEY}" > ${jumpbox_private_key_path}
 export BOSH_ALL_PROXY=ssh+socks5://${JUMPBOX_USERNAME}@${jumpbox_url}?private-key=${jumpbox_private_key_path}
 
-
 run_bats_on_vm() {
   stemcell_url=$1
-  bosh_release_path=$2
-  cpi_release_path=$3
-  garden_linux_release_path=$4
-  SKIP_RUBY_INSTALL=$5
-  BOSH_CLI_VERSION=$6
-  lite_director_ip="10.0.0.10"
+  iaas_stemcell_url=$2
+  iaas_stemcell_version=$3
+  bosh_release_path=$4
+  cpi_release_path=$5
+  garden_linux_release_path=$6
+  SKIP_RUBY_INSTALL=$7
+  BOSH_CLI_VERSION=$8
 
   export CREDHUB_PROXY=ssh+socks5://${JUMPBOX_USERNAME}@${jumpbox_url}?private-key=${jumpbox_private_key_path}
   export CREDHUB_SERVER=https://${BOSH_ENVIRONMENT}:8844
@@ -23,89 +23,104 @@ run_bats_on_vm() {
   echo "${CREDHUB_CA_PEM}" > ${CREDHUB_CA_CERT}
 
   credhub login --skip-tls-validation
-  deploy_director $stemcell_url $bosh_release_path $cpi_release_path $garden_linux_release_path $lite_director_ip
-  BOSH_LITE_CA_CERT="$(credhub get -n /concourse/bosh/default_ca -j | jq .value.ca -r)"
-  bosh -d bosh ssh -c "set -e -x; $(declare -f install_bats_prereqs); install_bats_prereqs $SKIP_RUBY_INSTALL $BOSH_CLI_VERSION"
-  bosh -d bosh ssh -c "set -e -x; $(declare -f run_bats); run_bats $lite_director_ip '$stemcell_url' '${BOSH_LITE_CA_CERT}'"
-  bosh -d bosh delete-vm $(bosh is --details --column=VM_CID) -n
-  bosh -d bosh delete-deployment -n
+  deploy_director $iaas_stemcell_url $iaas_stemcell_version $bosh_release_path $cpi_release_path $garden_linux_release_path
+  lite_director_ip="$(bosh -d bosh-warden-cpi-bats-director instances --json | jq -r '.Tables[].Rows[] | select( .instance | contains("bosh"))'.ips)"
 
+  BOSH_LITE_CA_CERT="$(credhub get -n /concourse/bosh/default_ca -j | jq .value.ca -r)"
+  bosh -d bosh-warden-cpi-bats-director ssh -c "set -e -x; $(declare -f install_bats_prereqs); install_bats_prereqs $SKIP_RUBY_INSTALL $BOSH_CLI_VERSION"
+  bosh -d bosh-warden-cpi-bats-director ssh -c "set -e -x; $(declare -f run_bats); run_bats $lite_director_ip '$stemcell_url' '${BOSH_LITE_CA_CERT}'"
+  bosh -d bosh-warden-cpi-bats-director delete-vm $(bosh is --details --column=VM_CID) -n
+  bosh -d bosh-warden-cpi-bats-director delete-deployment -n
 }
 
 deploy_director() {
-  stemcell_url=$1
+  iaas_stemcell_url=$1
+  iaas_stemcell_version=$2
   bosh_release_path=$2
-  cpi_release_path=$3
-  garden_linux_release_path=$4
-  lite_director_ip=$5
+  cpi_release_path=$4
+  garden_linux_release_path=$5
 
   # Upload specific dependencies
   bosh upload-release $bosh_release_path
   bosh upload-release $cpi_release_path
   bosh upload-release $garden_linux_release_path
+  bosh upload-stemcell $iaas_stemcell_url
 
+  # Deploy empty VM so we can get the IP
+  empty_manifest=$(mktemp)
+  cat <<EOF > $empty_manifest
+instance_groups:
+- azs:
+  - az1
+  instances: 1
+  jobs: []
+  name: bosh
+  networks:
+  - name: default
+  stemcell: default
+  vm_type: default
+name: bosh
+releases: []
+stemcells:
+- alias: default
+  os: ubuntu-bionic
+  version: $iaas_stemcell_version
+update:
+  canaries: 0
+  canary_watch_time: 60000
+  max_in_flight: 2
+  update_watch_time: 60000
+EOF
+
+  bosh -d bosh-warden-cpi-bats-director -n deploy $empty_manifest
+
+  lite_director_ip="$(bosh -d bosh-warden-cpi-bats-director instances --json | jq -r '.Tables[].Rows[] | select( .instance | contains("bosh"))'.ips)"
 
   git clone https://github.com/cloudfoundry/bosh-deployment.git
-  bd=./bosh-deployment
-    ops='
+  ops="
+  - type: replace
+    path: /releases/name=bosh/version
+    value: latest
+  - type: replace
+    path: /releases/name=bosh/version
+    value: latest
+  - type: replace
+    path: /instance_groups/name=bosh/azs?
+    value: [az1]
+  - type: remove
+    path: /instance_groups/name=bosh/jobs/name=disable_agent
+  - type: replace
+    path: /instance_groups/name=bosh/persistent_disk_type
+    value: large
+  - type: replace
+    path: /releases/name=bosh-warden-cpi/url
+    value: file://((local-bosh-warden-cpi-release))
+  - type: remove
+    path: /releases/name=bosh-warden-cpi/sha1
+  - type: replace
+    path: /releases/name=bosh-warden-cpi/version
+    value: latest
+  - type: remove
+    path: /instance_groups/name=bosh/networks/0/static_ips
+  - type: replace
+    path: /stemcells/0/version
+    value: $iaas_stemcell_url
+  - type: replace
+    path: /name
+    value: bosh-warden-cpi-bats-director
+  "
 
-    - type: replace
-      path: /releases/name=bosh/version
-      value: latest
-    - type: replace
-      path: /releases/name=bosh/version
-      value: latest
-    - type: replace
-      path: /instance_groups/name=bosh/azs?
-      value: [az1]
-    - type: replace
-      path: /stemcells/alias=default/os
-      value: ubuntu-bionic
-    - type: remove
-      path: /instance_groups/name=bosh/jobs/name=disable_agent
-    - type: replace
-      path: /instance_groups/name=bosh/persistent_disk_type
-      value: large
-    - type: replace
-      path: /releases/name=bosh-warden-cpi/url
-      value: file://((local-bosh-warden-cpi-release))
-
-    - type: remove
-      path: /releases/name=bosh-warden-cpi/sha1
-
-    - type: replace
-      path: /releases/name=bosh-warden-cpi/version
-      value: latest
-    '
-    bosh int ${bd}/bosh.yml  \
-        -o ${bd}/bosh-lite.yml \
-        -o ${bd}/misc/bosh-dev.yml \
-        -o <(echo -e "${ops}") \
-        -v internal_ip="$lite_director_ip" \
-        -v director_name="dev-director" \
-        -v local-bosh-warden-cpi-release="$cpi_release_path"
-    if $RECREATE ; then
-      RC='--recreate'
-    fi
-    if $SKIP_DRAIN ; then
-      SD='--skip-drain'
-    fi
-    if $FIX ; then
-      FF='--fix'
-    fi
-    bosh -d bosh -n deploy ${bd}/bosh.yml \
-        -o ${bd}/bosh-lite.yml \
-        -o ${bd}/misc/bosh-dev.yml \
-        -o <(echo -e "${ops}") \
-        -v internal_ip="$lite_director_ip" \
-        -v director_name="dev-director" \
-        -v admin_password=admin \
-        -v local-bosh-warden-cpi-release="$cpi_release_path" \
-        $RC $SD $FF
+  bosh -d bosh-warden-cpi-bats-director -n deploy ${bd}/bosh.yml \
+      -o ./bosh-deployment/bosh-lite.yml \
+      -o ./bosh-deployment/misc/bosh-dev.yml \
+      -o <(echo -e "${ops}") \
+      -v internal_ip="$lite_director_ip" \
+      -v director_name="dev-director" \
+      -v admin_password=admin \
+      -v local-bosh-warden-cpi-release="$cpi_release_path"
 }
 
 install_bats_prereqs() {
-  
   SKIP_RUBY_INSTALL=$1
   BOSH_CLI_VERSION=$2
   sudo apt-get -y update
@@ -168,7 +183,7 @@ run_bats() {
   export BAT_RSPEC_FLAGS=( --tag ~multiple_manual_networks --tag ~raw_instance_storage )
   export PATH=${PATH}:/var/vcap/store/bosh/bin/:/var/vcap/store/ruby/bin/
 
-  export BOSH_ENVIRONMENT=10.0.0.10
+  export BOSH_ENVIRONMENT=$lite_director_ip
   export BOSH_CLIENT=admin
   export BOSH_CLIENT_SECRET=admin
   export BOSH_CA_CERT="$BOSH_CA_CERT"
